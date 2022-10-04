7d5d36f2d9ac527a8e2f5957682dae5921bee646
Arc1el <khm970514@gmail.com>
2022-10-04 14:54:38 +0900

    using router, and add python dependencies

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
-from lib2to3.pytree import Base
-from tokenize import String
 from typing import Optional
 import uvicorn
 from fastapi import FastAPI
-from fastapi.requests import Request
-from fastapi.responses import JSONResponse
-from pydantic import BaseModel
-
-class Item(BaseModel):
-    time : str
-    temp : float
-    breath : int
-    rfid : str
+from routes.sensor import router as sensor_router
 
 app = FastAPI()
+app.include_router(sensor_router)
 
-@app.post("/sensors")
-async def get_sensor_data(data : Item):
-    return data.time
+@app.post("/")
+async def index():
+    return {"Hello":"World"}
---
+++ routes/sensor.py
@@ -0,0 +1,19 @@
+from typing import Optional
+import uvicorn
+from fastapi import FastAPI
+from fastapi import APIRouter, Depends
+from fastapi.requests import Request
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+
+router = APIRouter()
+
+class Item(BaseModel):
+    time : str
+    temp : float
+    breath : int
+    rfid : str
+
+@router.post("/sensor")
+async def get_sensor_data(data : Item):
+    return data.dict()
